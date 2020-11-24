cdef016ea1f0f551b5dd09f698b25db0c19fea9b
mumteshgarg <gargmumtesh97@gmail.com>
2020-11-24 10:41:01 +0530

    login complete

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,15 +10,9 @@ export class AuthService {
   constructor(public http: HttpClient) {}
 
   signInOrg(login) {
-    //if(login.role==="org"){
       return(this.http.post("http://localhost:8080/org/signin", login))
-    /*}
-    else if(login.role==="pat"){
-      return(this.http.post("http://localhost:8080/pat/signin", login))
-    }*/
   }
     signInPat(login){
-      console.log(login)
       return(this.http.post("http://localhost:8080/pat/signin", login))
     }
   patRegister(){
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -12,32 +12,38 @@ import { Login } from '../model/login';
 export class SigninComponent implements OnInit {
   login: Login;
   roledata: String
-  errorFlag : boolean
-  constructor(public auth: AuthService, public router : Router) {
+  errorFlag: boolean
+  constructor(public auth: AuthService, public router: Router) {
     this.login = new Login();
     this.roledata = ''
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
   loginSubmit(loginForm) {
     this.errorFlag = false
-    if(this.roledata==="org")
-    {
-      this.auth.signInOrg(this.login).subscribe(res => (console.log(res)))
+    if (this.roledata === "org") {
+      this.auth.signInOrg(this.login).subscribe((res: any) => {
+        if (res === null) {
+          this.errorFlag = true
+        }
+        else {
+          this.auth.data = res
+          console.log(this.auth.data)
+        }
+      })
     }
-    else if(this.roledata==="pat"){
-      this.auth.signInPat(this.login).subscribe(res => (console.log(res)))
+    else if (this.roledata === "pat") {
+      this.auth.signInPat(this.login).subscribe((res: any) => {
+        if (res === null) {
+          this.errorFlag = true
+        }
+        else {
+          this.auth.data = res
+          console.log(this.auth.data)
+        }
+      })
     }
-    /*this.auth.signIn(this.login,this.roledata).subscribe(res=>(console.log(res)) {
-      if (res === null) {
-        this.errorFlag = true
-      }
-      else{
-        this.auth.data = res
-        console.log(this.auth.data)
-      }
-    })
-    //this.login = new Login()
-    //loginForm.form.markAsPristine()*/
+    this.login = new Login()
+    loginForm.form.markAsPristine()
   }
 }
