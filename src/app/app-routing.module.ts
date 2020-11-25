9b85c4a11968ea43fb6df29557ff49bcdad688d5
mumteshgarg <gargmumtesh97@gmail.com>
2020-11-25 14:07:02 +0530

    routes edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { AddVaccComponent } from './add-vacc/add-vacc.component';
 import { DeletePatientComponent } from './delete-patient/delete-patient.component';
 import { EditProfilePatComponent } from './edit-profile-pat/edit-profile-pat.component';
 import { IndexComponent } from './index/index.component';
+import { OrgDeleteComponent } from './org-delete/org-delete.component';
 import { OrgEditProfileComponent } from './org-edit-profile/org-edit-profile.component';
 import { OrgHomeComponent } from './org-home/org-home.component';
 import { OrgRegisterComponent } from './org-register/org-register.component';
+import { OrgVaccComponent } from './org-vacc/org-vacc.component';
 import { PatHomeComponent } from './pat-home/pat-home.component';
 import { PatRegisterComponent } from './pat-register/pat-register.component';
 import { PatientReportComponent } from './patient-report/patient-report.component';
 import { RegisterComponent } from './register/register.component';
 import { SigninComponent } from './signin/signin.component';
+import { VaccEditComponent } from './vacc-edit/vacc-edit.component';
 
 const routes: Routes = [
   {
@@ -61,6 +65,22 @@ const routes: Routes = [
   {
     path : 'report/patient',
     component : PatientReportComponent
+  },
+  {
+    path : 'delete/org',
+    component : OrgDeleteComponent
+  },
+  {
+    path : 'org/vacc',
+    component : OrgVaccComponent
+  },
+  {
+    path : 'org/vacc/edit',
+    component : VaccEditComponent
+  },
+  {
+    path : 'org/vacc/add',
+    component : AddVaccComponent
   }
 ];
 
--- src/app/edit-profile-pat/edit-profile-pat.component.ts
+++ src/app/edit-profile-pat/edit-profile-pat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 import { Patient } from '../model/Patient';
 
 @Component({
@@ -11,7 +12,7 @@ export class EditProfilePatComponent implements OnInit {
   errorFlag : boolean
   successFlag : boolean
   pat : Patient
-  constructor() {
+  constructor(public auth: AuthService) {
     this.pat = new Patient()
     this.successFlag = false
     this.errorFlag = false
