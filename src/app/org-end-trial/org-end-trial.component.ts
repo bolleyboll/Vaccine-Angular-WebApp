f2377d4dfccc3d359a4012f55320b7c67feb4e35
Aman Gupta <31176934+bolleyboll@users.noreply.github.com>
2020-11-28 16:17:45 +0530

    Update org-end-trial.component.ts

--- src/app/org-end-trial/org-end-trial.component.ts
+++ src/app/org-end-trial/org-end-trial.component.ts
@@ -31,34 +31,6 @@ export class OrgEndTrialComponent implements OnInit {
       this.auth.vaccines = dbVaccine
     })
   }
-  /*
-   this.auth.getResult(this.patient.patientId).subscribe((dbReport: Report[]) => {
-     console.log(dbReport)
-     dbReport.forEach((res: Report) => {
-       if (res.result === "Under Trial") {
-         this.patientlist.push(res.patientId)
-       }
-     });*/
-
-  /*this.patientlist.forEach(_patientId => {
-    this.auth.getPatient(this.patient.patientId).subscribe((dbPatient: Patient) => {
-      this.patients.push(dbPatient)
-    })
-  });
-})
-}*/
-
-  /* patName(name) {
-     this.patname = name
-     for (let i = 0; i < this.patients.length; i++) {
-       if (this.patients[i].name === name) {
-         this.disname = this.patients[i].disease
-         this.gender = this.patients[i].gender
-         this.patient = this.patients[i]
-         break
-       }
-     }
-   }*/
   vaccName(name) {
     this.patients=[]
     this.vacname = name
@@ -66,13 +38,7 @@ export class OrgEndTrialComponent implements OnInit {
       if (name === vacc.name) {
         this.vaccid =vacc.vaccId
       }
-    });/*
-    for (let i = 0; i < this.auth.vaccines.length; i++) {
-      if (name === this.auth.vaccines[i].name) {
-        this.vaccid = this.auth.vaccines[i].vaccId
-        break
-      }
-    }*/
+    });
     console.log(this.patients)
     this.auth.getVacReport(this.vaccid).subscribe((res: Report[]) => {
       if (res.length == 0) {
@@ -80,23 +46,14 @@ export class OrgEndTrialComponent implements OnInit {
         this.patients=[]
       }
       else {
-        res.forEach(result => {
+        res.forEach((result) => {
           if(result.result==="Under Trial"){
             this.reports.push(result)
+            this.auth.getPatient(result.patientId).subscribe((pat: Patient) =>{
+              this.patients.push(pat)
+            })
           }
-        });/*
-        for (let i = 0; i < res.length; i++) {
-          if (res[i].result === "Under Trial") {
-            this.reports.push(res[i]) 
-          }
-        }
-        console.log(this.patients)
-
-        for(let j=0;j<this.reports.length;j++){
-          this.auth.getPatient(this.reports[j].patientId).subscribe((res:Patient) =>{
-            this.patients[j]=res
-          })
-        }*/
+        });
         console.log(this.patients)
       }
     })
