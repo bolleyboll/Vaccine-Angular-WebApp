2882f2aabea66ab5de8da8430c70702f7583266d
mumteshgarg <gargmumtesh97@gmail.com>
2020-11-28 09:19:06 +0530

    patient reports

--- src/app/patient-report/patient-report.component.ts
+++ src/app/patient-report/patient-report.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Organization } from '../model/Organization';
 import { Report } from '../model/Report';
+import { Vaccine } from '../model/Vaccine';
 
 @Component({
   selector: 'app-patient-report',
@@ -14,10 +16,18 @@ export class PatientReportComponent implements OnInit {
   patid:number
   orgid:number
   vaccid:number
+  orgdetail: Organization
+  vaccdetail :Vaccine
+  successFlag: boolean
+  textcolor:String
 
   constructor(public auth: AuthService) {
     this.report = new Report()
+    this.orgdetail = new Organization()
+    this.vaccdetail = new Vaccine()
     this.num = ''
+    this.errorFlag=false
+    this.successFlag=false
   }
 
   ngOnInit(): void {
@@ -31,11 +41,27 @@ export class PatientReportComponent implements OnInit {
     })
   }
   setId(id) {
+    this.successFlag=true
     this.num = id
     this.patid=this.auth.reports[id-1].patientId
     this.orgid=this.auth.reports[id-1].orgId
     this.vaccid=this.auth.reports[id-1].vaccId
-    this.auth.getOrg(this.orgid).subscribe(res => console.log(res))
-    this.auth.getVacc(this.vaccid).subscribe(res =>console.log(res))
+    this.report.result=this.auth.reports[id-1].result
+    this.report.description=this.auth.reports[id-1].description
+    if(this.report.result==="Pass"){
+      this.textcolor="text-success"
+    }
+    else if(this.report.result==="Fail"){
+      this.textcolor="text-danger"
+    }
+    else{
+      this.textcolor="text-info"
+    }
+    this.auth.getOrg(this.orgid).subscribe((res : any) => {
+      this.orgdetail=res
+    })
+    this.auth.getVacc(this.vaccid).subscribe((res : any) => {
+      this.vaccdetail=res
+    })
   }
 }
